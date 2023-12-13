d4340128ce97f8ca092d62a87b8d9bb68cbf60ee
martintxoz <30549657+martintxoz@users.noreply.github.com>
2023-12-13 12:01:53 +0100

    New version with some fixes: icons, inline script fixes...

--- mi-updateador.sh
+++ mi-updateador.sh
@@ -75,6 +75,12 @@ fi
 		export yad_update_title2="Update "$NUPGRADEABLE" package(s)? Some are IN USE!!"
 		export yad_update_text2="\nDo you want to update <b>"$NUPGRADEABLE" software package(s)</b>?\n \nThe following applications seems to be <b>currently RUNNING</b>: "$PAKETESCORRIENDO".\n \nI think it is important that <b>AFTER THE UPDATE</b>:\n - You turn off and run these applications again, if you know what they are.\n - If you do not know them, exit from the session or reboot the PC.\nIf you update, I will leave an icon for you to remember, if you click on it, you can turn off...\n \nThe "$NUPGRADEABLE" package(s) that are going to be updated are the following:"
 		export reboot_text="AT THE END OF THE UPDATE, remember to turn off applications started:"$PAKETESCORRIENDO", or logout or reboot computer (left click on icon to logout/reboot, right click for kill icon)"
+		export apt_update_text_yes="Look for new packages:"
+		export apt_update_text_no="Don't look for new packages."
+		export apt_installation_full_text="Do complete installation:"
+		export apt_installation_text="Don't do complete installation:"
+		export apt_autoremove_yes_text="Uninstall unneeded packages:"
+		export apt_autoremove_no_text="Don't uninstall unneeded packages."
 		export upgrade_completed="Full upgrade complete (or was canceled)."
 		export terminal_close="This terminal window can now be closed."
 		export key_close="Press any key to close window"
@@ -85,7 +91,7 @@ fi
 #Other languages:		
 case $LANG in
     #Spanish
-	es* )
+	eu* )
 		export software_update="Actualizar_software"
 		export no_updates="NO hay actualizaciones de software disponibles"
 		export yes_updates1="Hay "$NUPGRADEABLE" actualizacion(es) de software disponible(s)."
@@ -96,6 +102,12 @@ case $LANG in
 		export yad_update_title2="¿Actualizar "$NUPGRADEABLE" paquete(s)? Algunos EN USO!!"
 		export yad_update_text2="\n¿Quieres actualizar <b>"$NUPGRADEABLE" paquete(s)</b> de software?\n \nDe esos programas los siguientes parece que <b>estan ARRANCADOS</b> actualmente:\n"$PAKETESCORRIENDO"\n \nCreo que es importante que <b>DESPUES DE la actualización</b>:\n - Apagues y vuelvas a arrancar esos programas, si sabes cuales son.\n - Si no los conoces, sal de la sesion o reinicia el ordendador.\nSi actualizas, dejare un icono para que te acuerdes, si lo pinchas puedes apagar...\n \nLos "$NUPGRADEABLE" paquete(s) que se van a actualizar son los siguientes:"
 		export reboot_text="AL ACABAR LA ACTUALIZACION, acuerdate de apagar programas arrancados: "$PAKETESCORRIENDO", o de salir o reiniciar ordenador (click izquierdo en icono para salir/reiniciar, click derecho para quitar icono)"
+		export apt_update_text_yes="Buscamos nuevos paquetes:"
+		export apt_update_text_no="No buscamos nuevos paquetes."
+		export apt_installation_full_text="Hacemos instalacion completa:"
+		export apt_installation_text="No hacemos instalacion completa:"
+		export apt_autoremove_yes_text="Desinstalamos paquetes no necesarios:"
+		export apt_autoremove_no_text="No desinstalamos paquetes no necesarios."
 		export upgrade_completed="Se realizó la actualización completa (o fue cancelada)."
 		export terminal_close="Esta ventana de terminal ya puede cerrarse."
 		export key_close="Oprima cualquier tecla para cerrar esta ventana"
@@ -109,18 +121,44 @@ esac
 
 #Function showing the window that reports the packages that can be upgraded. 
 # This function is called by icon in the taskbar, at script last lines: yadicono ... 
-segundo-paso-ventana-y-upgrade()
+segundo-paso-ventana-y-upgrade ()
 {
 
 #Another function for no to repeat this code in two places. Run "apt upgrade" in terminal.
 crear-inline-script-y-ejecutar ()
 {
 #The following lines, until the last EOF, is the code for the update script in /tmp 
+# Primera parte del upgrade script...
 cat > /tmp/mi-updateador.sh << "EOF"
 #!/bin/bash
 
+#Pequeño script temporal en /tmp/mi-updateador.sh para upgradear el sistema.
+# Cuando todo el asunto termina se supone que este script se borra...
+# Si no ha sido asi, algo ha ido mal...
+
 #Turn on script debugging:
 #set -x
+
+#Variables establecidas en el script principal /usr/local/bin/mi-updateador.sh
+EOF
+
+#Enviar variables al inline script empezado a hacer en /tmp/mi-updateador.sh
+echo "apt_update=\"$apt_update\"" >> /tmp/mi-updateador.sh
+echo "apt_installation=\"$apt_installation\"" >> /tmp/mi-updateador.sh
+echo "apt_autoremove=\"$apt_autoremove\"" >> /tmp/mi-updateador.sh
+echo "software_update=\"$software_update\"" >> /tmp/mi-updateador.sh
+echo "upgrade_completed=\"$upgrade_completed\"" >> /tmp/mi-updateador.sh
+echo "terminal_close=\"$terminal_close\"" >> /tmp/mi-updateador.sh
+echo "apt_update_text_yes=\"$apt_update_text_yes\"" >> /tmp/mi-updateador.sh
+echo "apt_update_text_no=\"$apt_update_text_no\"" >> /tmp/mi-updateador.sh
+echo "apt_installation_full_text=\"$apt_installation_full_text\"" >> /tmp/mi-updateador.sh
+echo "apt_installation_text=\"$apt_installation_text\"" >> /tmp/mi-updateador.sh
+echo "apt_autoremove_yes_text=\"$apt_autoremove_yes_text\"" >> /tmp/mi-updateador.sh
+echo "apt_autoremove_no_text=\"$apt_autoremove_no_text\"" >> /tmp/mi-updateador.sh
+echo "key_close=\"$key_close\"" >> /tmp/mi-updateador.sh
+
+#2º parte del inline script, continuar con el inline script...
+cat >> /tmp/mi-updateador.sh << "EOF"
 
 #Without this, the commands to change window size do not work, or affect others, not this terminal...
 sleep 0.25s
@@ -136,19 +174,19 @@ wmctrl -r "$software_update" -e 0,$(($sw/2-$ww/2)),$(($sh/2-$wh/2)),$ww,$wh
 
 #Compose the APT command line from the settings up in the original script
 if [ "$apt_update" = "yes" ]; then
-  export apt_update="echo ' ';echo 'Look for new packages/Buscamos nuevos paquetes:';echo '==============================================';echo ' ';apt update;"
+  apt_update="echo ' ';echo '$apt_update_text_yes';echo '============================';echo ' ';apt update;"
 else
-  export apt_update="echo ' ';echo 'Dont look for new packages/No buscamos nuevos paquetes.';echo '==============================================';echo ' ';"
+  apt_update="echo ' ';echo '$apt_update_text_no';echo '============================';echo ' ';"
 fi
 if [ "$apt_installation" = "apt-full-upgrade" ]; then
-  export apt_installation="echo ' ';echo 'Do complete installation/Hacemos instalacion completa:';echo '==============================================';echo ' ';apt -V full-upgrade;"
+  apt_installation="echo ' ';echo '$apt_installation_full_text';echo '=============================';echo ' ';apt -V full-upgrade;"
 else
-  export apt_installation="echo ' ';echo 'Dont do complete installation/No hacemos instalacion completa:';echo '==============================================';echo ' ';apt -V upgrade;"
+  apt_installation="echo ' ';echo '$apt_installation_text';echo '=============================';echo ' ';apt -V upgrade;"
 fi
 if [ "$apt_autoremove" = "yes" ]; then
-  export apt_autoremove="echo ' ';echo 'Uninstall unneeded packages/Desinstalamos paquetes no necesarios:';echo '==============================================';echo ' ';apt -V autoremove --purge;"
+  apt_autoremove="echo ' ';echo '$apt_autoremove_yes_text';echo '===================================';echo ' ';apt -V autoremove --purge;"
 else
-  export apt_autoremove="echo ' ';echo 'Dont uninstall unneeded packages/No desinstalamos paquetes no necesarios.';echo '==============================================';echo ' ';"
+  apt_autoremove="echo ' ';echo '$apt_autoremove_no_text';echo '===================================';echo ' ';"
 fi
 
 #The commands that really make the update (variables determined up) and:
@@ -162,19 +200,22 @@ echo
 echo "$upgrade_completed"
 echo
 echo "$terminal_close"' '
-#read -sn 1 -p '("$key_close")' -t 999999999
+#read -sn 1 -p '($key_close)' -t 999999999
 read -sn 1 -p '(Any key closes / Cualquier tecla cierra)' -t 999999999
 
 #Turn off script debugging:
 #set +x
 
 EOF
+
 chmod 755 /tmp/mi-updateador.sh
 #Wait to write the script on disk and execute it in a terminal
 sleep 0.5s
-x-terminal-emulator -T $software_update -e /tmp/mi-updateador.sh
+#x-terminal-emulator -T $software_update -e /tmp/mi-updateador.sh && wait $!
+#Con mi roxterm no consigo que se espere a completar, con xterm si...
+#xterm -fg white -fa 'Monospace' -fs 11 -T $software_update -e /tmp/mi-updateador.sh
+termit --init /home/martintxo/.config/termit/rc.lua-NORMAL --title $software_update --name Informaciones -e /tmp/mi-updateador.sh
 
-rm /tmp/mi-updateador*
 }
 #"Apt upgrade script" function end
 
@@ -190,22 +231,24 @@ yad --window-icon=system-software-update \
         --image=system-software-update \
         --title "$yad_update_title1" \
         --text="$yad_update_text1" \
-        --form --field :TXT "$(apt-get -o Debug::NoLocking=true --trivial-only -V dist-upgrade 2>/dev/null | tail -n +5)" \
-        --button gtk-ok:0 --button gtk-cancel:1 >& /dev/null
+        --form --field :TXT "$(cat /tmp/mi-updateador | tail -n +5)" \
+        --button=gtk-ok:0 --button=gtk-cancel:1 >& /dev/null
 
   #If click in OK run the updater (function: crear-inline-script-y-ejecutar, above),
   # otherwise close the window and wait (icon in taskbar)
   #  (If something is modified here, it will also be modified below after "else")
   if [ "$?" = "0" ]; then
-
+    
+    #Execute the update function, above
+    crear-inline-script-y-ejecutar
+    
     #Remove the icon on the system tray (taskbar) 
     # The code here has to be the same as down, in $yadicono...
     pkill --full 'yad --notification --image=system-software-update --text=$yes_updates3 --command=bash -c segundo-paso-ventana-y-upgrade --menu=Actualizar!bash -c segundo-paso-ventana-y-upgrade!system-software-update|Buscar actualizaciones!bash -c user-apt-update!reload|Historia Dpkg!bash -c dpkg-history-window!emblem-debian|Historia Apt!bash -c apt-history-window!emblem-debian-symbolic|Salir!quit!back'
-    
-    #Execute the update function
-    crear-inline-script-y-ejecutar
-    exit 0
-    
+
+    rm /tmp/mi-updateador*
+    exit
+
   fi
 
 #If there are "packages to update and running", shows a window explaining the problem and with the packages list:
@@ -217,8 +260,8 @@ yad --window-icon=system-software-update \
         --image=software-update-urgent \
         --title "$yad_update_title2" \
         --text="$yad_update_text2" \
-        --form --field :TXT "$(apt-get -o Debug::NoLocking=true --trivial-only -V dist-upgrade 2>/dev/null | tail -n +5)" \
-        --button gtk-ok:0 --button gtk-cancel:1 >& /dev/null
+        --form --field :TXT "$(cat /tmp/mi-updateador | tail -n +5)" \
+        --button=gtk-ok:0 --button=gtk-cancel:1 >& /dev/null
 
   #If click in OK run the updater (function: crear-inline-script-y-ejecutar, above),
   # otherwise close the window and wait (icon in taskbar)
@@ -230,15 +273,18 @@ yad --window-icon=system-software-update \
     pkill --full 'yad --notification --image=system-software-update --text=$yes_updates3 --command=bash -c segundo-paso-ventana-y-upgrade --menu=Actualizar!bash -c segundo-paso-ventana-y-upgrade!system-software-update|Buscar actualizaciones!bash -c user-apt-update!reload|Historia Dpkg!bash -c dpkg-history-window!emblem-debian|Historia Apt!bash -c apt-history-window!emblem-debian-symbolic|Salir!quit!back'
 
     #This puts an icon in system tray to remember reboot... if click on it starts the logout app (configured above)
-    yad --notification --image=software-update-urgent --text="$reboot_text" --command="$reboot_app" --menu='Quit/Salir!quit!back' &
+    yad --notification --image=software-update-urgent --text="$reboot_text" --command="$reboot_app" --menu='Salir-Reiniciar!/usr/local/bin/obshutdown!software-update-urgent|Quitar-icono!quit!back' &
     
     #Execute the update function
     crear-inline-script-y-ejecutar
-    exit 0
+
+    rm /tmp/mi-updateador*
+    exit
+
   fi
 
 fi
-rm /tmp/mi-updateador*
+
 #Turn off script debugging:
 #set +x
 }
@@ -253,12 +299,23 @@ export -f segundo-paso-ventana-y-upgrade
 apt-history-window()
 {
 #Window with history:
-cat /var/log/apt/history.log | yad --window-icon=system-software-update \
-         --text-info --width=1000 --height=500 --center --button=gtk-close \
+if [ -s /var/log/apt/history.log ]; then
+  cat /var/log/apt/history.log | yad --window-icon=system-software-update \
+         --text-info --width=1000 --height=500 --center \
+	     --button=gtk-cancel:1 \
          --image=system-software-update \
          --title "$apt_history_title" \
          --text="$apt_history_window" \
          --fontname="mono regular 11" --margins=5 --borders=1 >& /dev/null
+else
+  echo "El archivo de log está vacio / Log file is empty." | yad --window-icon=system-software-update \
+         --text-info --width=1000 --height=500 --center \
+	     --button=gtk-cancel:1 \
+         --image=system-software-update \
+         --title "$apt_history_title" \
+         --text="$apt_history_window" \
+         --fontname="mono regular 11" --margins=5 --borders=1 >& /dev/null
+fi
 }
 #Export the "Apt history window" function so that the Yad icon can execute it 
 export -f apt-history-window
@@ -269,12 +326,23 @@ export -f apt-history-window
 dpkg-history-window()
 {
 #Window with history:
+if [ -s /var/log/dpkg.log ]; then
 zgrep -EH ' install | upgrade | purge | remove ' /var/log/dpkg.log | cut -f2- -d: | sort -r | sed 's/ remove / remove  /;s/ purge / purge   /' | grep "^" | yad --window-icon=system-software-update \
-         --text-info --width=1000 --height=500 --center --button=gtk-close \
+         --text-info --width=1000 --height=500 --center \
+	     --button=gtk-cancel:1 \
          --image=system-software-update \
          --title "$dpkg_history_title" \
          --text="$dpkg_history_window" \
          --fontname="mono regular 11" --margins=5 --borders=1 >& /dev/null
+else
+  echo "El archivo de log está vacio / Log file is empty." | yad --window-icon=system-software-update \
+         --text-info --width=1000 --height=500 --center \
+	     --button=gtk-cancel:1 \
+         --image=system-software-update \
+         --title "$dpkg_history_title" \
+         --text="$dpkg_history_window" \
+         --fontname="mono regular 11" --margins=5 --borders=1 >& /dev/null
+fi
 }
 #Export the "Dpkg history window" function so that the Yad icon can execute it 
 export -f dpkg-history-window
@@ -285,11 +353,28 @@ export -f dpkg-history-window
 user-apt-update()
 {
 #The following lines, until the last EOF, is the code for the update script in /tmp 
+# primera parte del update script...
 cat > /tmp/user-apt-update.sh << "EOF"
 #!/bin/bash
 
+#Pequeño script temporal en /tmp para updatear la lista de paquetes del sistema.
+# Cuando todo el asunto termina se supone que este script se borra...
+# Si no ha sido asi, algo ha ido mal...
+
 #Turn on script debugging:
 #set -x
+
+#Variables establecidas en el script principal /usr/local/bin/mi-updateador.sh
+EOF
+
+#Enviar variables al inline script empezado a hacer en /tmp/mi-updateador.sh
+echo "software_update=\"$software_update\"" >> /tmp/user-apt-update.sh
+echo "apt_update_text_yes=\"$apt_update_text_yes\"" >> /tmp/user-apt-update.sh
+echo "terminal_close=\"$terminal_close\"" >> /tmp/user-apt-update.sh
+echo "key_close=\"$key_close\"" >> /tmp/user-apt-update.sh
+
+#2º parte del inline script, continuar con el inline script...
+cat >> /tmp/user-apt-update.sh << "EOF"
 
 #Without this, the commands to change window size do not work, or affect others, not this terminal...
 sleep 0.25s
@@ -304,7 +389,7 @@ read wx wy ww wh < <(wmctrl -lG | grep "$software_update" | sed 's/^[^ ]* *[^ ]*
 wmctrl -r "$software_update" -e 0,$(($sw/2-$ww/2)),$(($sh/2-$wh/2)),$ww,$wh
 
 #Compose the APT command line for apt update
-export apt_update_user="echo ' ';echo 'Look for new packages/Buscamos nuevos paquetes:';echo '==============================================';echo ' ';apt update;"
+export apt_update_user="echo ' ';echo '$apt_update_text_yes';echo '=========================';echo ' ';apt update;"
 
 #The commands that really make the update (variables determined up) and:
 # - pkexec (from polkit) for gain root
@@ -315,17 +400,20 @@ pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY bash -c "$apt_update_user"
 #Terminal window remains open until any key is pressed and closes it 
 echo
 echo "$terminal_close"' '
-#read -sn 1 -p '("$key_close")' -t 999999999
+#read -sn 1 -p '($key_close)' -t 999999999
 read -sn 1 -p '(Any key closes / Cualquier tecla cierra)' -t 999999999
 
 #Turn off script debugging:
 #set +x
+
+exit
 
 EOF
 chmod 755 /tmp/user-apt-update.sh
 #Wait to write the script on disk and execute it in a terminal
 sleep 0.5s
 #x-terminal-emulator -T $software_update -e /tmp/user-apt-update.sh
+#xterm -fg white -fa 'Monospace' -fs 11 -T $software_update -e /tmp/user-apt-update.sh
 termit --title $software_update --name Informaciones -e /tmp/user-apt-update.sh
 
 rm /tmp/user-apt-update.sh
@@ -347,7 +435,6 @@ if [ "$NUPGRADEABLE" = "" ] || [ "$NUPGRADEABLE" = "0" ] && [ "$USER" = "" ]; th
 
     notify-send --app-name="$software_update" --icon="$notification_icon" --urgency=low "$no_updates"
     rm /tmp/mi-updateador*
-    exit 0
 
 else
 
@@ -365,5 +452,3 @@ fi
 
 #Turn off script debugging:
 #set +x
-
-exit 0
